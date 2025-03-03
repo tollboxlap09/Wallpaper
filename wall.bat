7bf917afa05465ac92cef88ced890319d829870c
tollboxlap09 <eslammohamdeen89@gmail.com>
2025-03-04 01:16:56 +0200

    Update wall.bat

--- wall.bat
+++ wall.bat
@@ -1,14 +1,11 @@
 @echo off
-powershell -Command "Invoke-WebRequest https://github.com/tollboxlap09/Wallpaper/blob/main/TranscodedWallpaper -OutFile TranscodedWallpaper"
-powershell -Command "Invoke-WebRequest https://github.com/tollboxlap09/Wallpaper/blob/main/TOOLBOXLAP.jpg"
+powershell -Command "Invoke-WebRequest https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/CachedImage_1024_768_POS4.jpg -OutFile CachedImage_1024_768_POS4.jpg"
 
-set "TranscodedWallpaper=TranscodedWallpaper"
-set "CachedImage=TOOLBOXLAP.jpg"
+set "CachedImage=CachedImage_1024_768_POS4.jpg"
 
 set "destinationDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes"
 set "cachedFileDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles"
 
-copy /y "%TranscodedWallpaper%" "%destinationDir%\TranscodedWallpaper"
-copy /y "%CachedImage%" "%cachedFileDir%\TOOLBOXLAP.jpg"
+copy /y "%CachedImage%" "%cachedFileDir%\CachedImage_1024_768_POS4.jpg"
 
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
